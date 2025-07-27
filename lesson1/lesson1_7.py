744e8bfbf15d3eb0904054fb40773c85edeb2c47
Annawer <q5j6jp6m3@gmail.com>
2025-07-27 17:12:30 +0800

    色彩

--- lesson1/lesson1_7.py
+++ lesson1/lesson1_7.py
@@ -4,8 +4,12 @@ import cv2
 cv2.namedWindow("MyPicture")
 img = cv2.imread('coca-cola-logo.png')
 cv2.imshow("MyPicture", img)
+
+# 儲存圖片
+# 將圖片儲存為coca-cola-logo-copy.jpg
 ret = cv2.imwrite('coca-cola-logo-copy.jpg', img)
 
+# 檢查儲存是否成功
 if ret:
     print("儲存檔案成功!")
 else:

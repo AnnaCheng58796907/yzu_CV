4d031bbb689443ccefa5b8888ec412ed613a734f
Annawer <q5j6jp6m3@gmail.com>
2025-08-03 15:47:45 +0800

    homework
    Signed-off-by: Annawer <q5j6jp6m3@gmail.com>

--- lesson2/homework.py
+++ lesson2/homework.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 img = cv2.imread('mountain.jpg')
 hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -8,50 +9,37 @@ cv2.imshow('mountain', img)
 ret_value = cv2.waitKey(0)
 while ret_value != ord('q'):
     if ret_value == ord('w'):
-        h[:, :] += 5
-        hsv_img = cv2.merge([h, s, v])
-        cv2.imshow('mountain', hsv_img)
-        if h.fill == 179:
+        if np.all(h > 179):
             h.fill(179)
-            hsv_img = cv2.merge([h, s, v])
-            cv2.imshow('mountain', hsv_img)
+        else:
+            h[:, :] += 5
     elif ret_value == ord('s'):
-        h.fill -= 5
-        if h == 0:
-            h.fill = 0
+        if np.all(h < 0):
+            h.fill(0)
+        else:
+            h[:, :] -= 5
     elif ret_value == ord('e'):
-        s += 5
-
+        s.fill += 5
+        if s.fill > 255:
+            s.fill = 255
+    elif ret_value == ord('d'):
+        s.fill -= 5
+        if s.fill < 0:
+            s.fill = 0
+    elif ret_value == ord('r'):
+        v.fill +=5
+        if v.fill > 255:
+            v.fill = 255
+    elif ret_value == ord('f'):
+        v.fill -=5
+        if v.fill < 0:
+            v.fill = 0
+    elif ret_value == ord('y'):
+        hsv_img = img
+        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(hsv_img)
+    hsv_img = cv2.merge([h, s, v])
+    cv2.imshow('mountain', hsv_img)
     cv2.waitKey(0)
 
-cv2.destroyWindow('montain')    
-
-'''
-def bgr_fill():
-
-    pass
-
-
-def hsv_fill():
-    pass
-
-
-def a_fill():
-    pass
-
-
-def main():
-    ret_value = cv2.waitKey(0)
-    if ret_value == ord('h'):
-        hsv_fill()
-    elif ret_value == ord('a'):
-        a_fill()
-    elif ret_value == ord('y'):
-        bgr_fill(img)
-    elif ret_value == ord('q'):
-        cv2.destroyAllWindows()
-
-
-if __name__ == "__main__":
-    main()
-'''
+cv2.destroyAllWindows()

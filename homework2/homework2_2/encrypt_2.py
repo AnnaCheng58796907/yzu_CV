7809df162067bcf9b8cd2da56fec4cfd4bffb02b
Annawer <q5j6jp6m3@gmail.com>
2025-08-10 22:17:47 +0800

    homework
    Signed-off-by: Annawer <q5j6jp6m3@gmail.com>

--- homework2/homework2_2/encrypt_2.py
+++ homework2/homework2_2/encrypt_2.py
@@ -8,14 +8,9 @@ h = pt_y // 2
 w = pt_x // 2
 key = np.random.randint(0, 256, [h, w, 3], np.uint8)
 
-src_encry = [(0, 0),
-             (0, pt_x - w),
-             (pt_y - h, 0),
-             (pt_y - h, pt_x - w)
-             ]
+src_key = np.tile(key, (2, 2, 1))
 
-for i, j in src_encry:
-    src[i:i+h, j:j+w] = key
+src_encry = cv2.bitwise_xor(src, src_key)
 
 cv2.imwrite("key_2.png", key)
-cv2.imwrite("encrypted_2.png", src)
+cv2.imwrite("encrypted_2.png", src_encry)

528bebe32099ab3eeae0b559d113f7e915ed97d1
Annawer <q5j6jp6m3@gmail.com>
2025-08-24 19:13:07 +0800

    整理
    Signed-off-by: Annawer <q5j6jp6m3@gmail.com>

--- lesson4/lesson4_1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-image = np.zeros((5, 12), np.uint8)
-print(f"修改前 image = \n{image}")
-print(f"image[1, 4] = {image[1, 4]}")
-print("=======分隔線=======")
-image[1, 4] = 255
-print(f"修改后 image = \n{image}")
-print(f"image[1, 4] = {image[1, 4]}")

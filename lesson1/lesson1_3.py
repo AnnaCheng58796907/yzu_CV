3e62bc63ecd7ef465f6efa526abcd9df1dc381eb
Annawer <q5j6jp6m3@gmail.com>
2025-08-24 15:27:18 +0800

    整理
    Signed-off-by: Annawer <q5j6jp6m3@gmail.com>

--- lesson1/lesson1_3.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread('coca-cola-logo.png')
-
-cv2.imshow("MyPicture", img)
-ret_value = cv2.waitKey(5000)  # 0是無限等待，5000是等待5秒
-cv2.destroyWindow("MyPicture")
-cv2.destroyAllWindows()
